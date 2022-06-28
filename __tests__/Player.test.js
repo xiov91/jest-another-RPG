eefbc143d518f1e2ae522ae5163472a80b7d9d4c
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-06-28 18:19:09 -0400

    getStats added

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -5,7 +5,6 @@ jest.mock('../lib/Potion');
 console.log(new Potion());
 
 const Player = require('../lib/Player');
-const { JestHook } = require('jest-watcher');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -18,3 +17,22 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
     );
 });
+
+test("gets player's stats as an object", () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
